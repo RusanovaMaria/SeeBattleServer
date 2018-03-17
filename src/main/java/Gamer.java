03c347860db7330f035fb16618bbbe7554419c9c
Mary <mariarusanova1997@mail.ru>
2018-03-17 17:36:43 +0300

    Добавлена функция приема сообщений от клиент ов

--- src/main/java/Gamer.java
+++ src/main/java/Gamer.java
@@ -1,40 +1,43 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-public class Gamer implements Runnable{
+public class Gamer implements Runnable {
     private Socket socket;
     DataInputStream inStream;
     DataOutputStream outStream;
 
-    Gamer (Socket gamerSocket) {
+    Gamer(Socket gamerSocket) {
         try {
             socket = gamerSocket;
             inStream = new DataInputStream(gamerSocket.getInputStream());
             outStream = new DataOutputStream(gamerSocket.getOutputStream());
-
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("Gamer constructor error");
             ex.printStackTrace();
         }
     }
+
     public void run() {
 
     }
 
-    public void sendMessage (String message){
+    public void sendMessage(String message) {
         try {
             outStream.writeUTF(message);
             outStream.flush();
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("Ошибка правки сообщения");
             ex.printStackTrace();
         }
     }
 
-    public Scanner gerScanner(){
-        return scanner;
+    public String getMessage() {
+        String message = "";
+        try {
+            message = inStream.readUTF();
+        } catch (IOException ex) {
+            System.out.println("Ошибка получения сообщения");
+        }
+        return message;
     }
 }
