81e00deb4906f821f6bb39bc8519da6b55f5e069
Mary <mariarusanova1997@mail.ru>
2018-03-17 17:52:31 +0300

    Низкоуровневые детали класса Gamer перенесены в класс GameHandler

---
+++ src/main/java/GamerHandler.java
@@ -0,0 +1,29 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class GamerHandler {
+
+    private Socket socket;
+    private DataInputStream inStream;
+    private DataOutputStream outStream;
+
+    GamerHandler(Socket gamerSocket) {
+        try {
+            socket = gamerSocket;
+            inStream = new DataInputStream(gamerSocket.getInputStream());
+            outStream = new DataOutputStream(gamerSocket.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println("Gamer constructor error");
+            ex.printStackTrace();
+        }
+    }
+    public DataInputStream getInStream() {
+        return inStream;
+    }
+
+    public DataOutputStream getOutStream() {
+        return outStream;
+    }
+}
